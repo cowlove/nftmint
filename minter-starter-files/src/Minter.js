aa0c3adf261b41a2c3153b0eded85f0908332fbb
jim <jim@vheavy.com>
2021-09-15 08:38:52 -0700

    Finished tutorial

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { connectWallet, mintNFT, getCurrentWalletConnected } from "./utils/interact.js";
 
 const Minter = (props) => {
 
@@ -9,16 +10,50 @@ const Minter = (props) => {
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
  
-  useEffect(async () => { //TODO: implement
-    
+  useEffect(async () => { 
+      const {address, status} = await getCurrentWalletConnected();
+      setWallet(address)
+      setStatus(status); 
+
+      addWalletListener(); 
+
   }, []);
 
   const connectWalletPressed = async () => { //TODO: implement
-   
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address); 
   };
 
-  const onMintPressed = async () => { //TODO: implement
-    
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+  
+
+  const onMintPressed = async () => {
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);  
   };
 
   return (
@@ -68,5 +103,6 @@ const Minter = (props) => {
     </div>
   );
 };
+
 
 export default Minter;
